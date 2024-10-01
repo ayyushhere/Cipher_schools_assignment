4be90ca334af069d009c7f602639c257e40958e6
Ayush Kumar <ayushkumar0211a@gmail.com>
2024-10-01 17:09:07 +0530

    Day 18

---
+++ DAY 18/Ques1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* createNode(int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->next = nullptr;
+    return newNode;
+}
+
+void insertInSortedOrder(Node*& head, int value) {
+    Node* newNode = createNode(value);
+
+    if (head == nullptr || head->data >= value) {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    Node* current = head;
+    while (current->next != nullptr && current->next->data < value) {
+        current = current->next;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data;
+        if (temp->next != nullptr)
+            cout << "->";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = createNode(10);
+    head->next = createNode(20);
+    head->next->next = createNode(30);
+    head->next->next->next = createNode(40);
+
+    int value = 25;
+
+    cout << "Original list: ";
+    printList(head);
+
+    insertInSortedOrder(head, value);
+
+    cout << "List after insertion: ";
+    printList(head);
+
+    return 0;
+}
