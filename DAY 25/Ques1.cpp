fef01f9324b2b5f70e17f52ad54cdef84e5b8901
Ayush Kumar <ayushkumar0211a@gmail.com>
2024-10-08 09:30:04 +0530

    Day 24 ques 1

---
+++ DAY 25/Ques1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findSingleNumber(vector<int>& nums) {
+    int result = 0;
+    for (int num : nums) {
+        result ^= num;
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    cout << findSingleNumber(nums) << endl;
+    return 0;
+}
